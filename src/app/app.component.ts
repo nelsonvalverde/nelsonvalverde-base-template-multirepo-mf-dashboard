2755d2330a140b50c44299916e77b5568a63c181
Nelson Valverde La Torre <nelsonvalverdelt@aysconsultores.pe>
2024-08-10 01:53:45 -0500

    avance de impl de dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { ChangeDetectionStrategy, Component, OnInit, signal, WritableSignal } from '@angular/core';
+import { AccordionModule } from 'primeng/accordion';
+import Chart from 'chart.js/auto';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AccordionModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'mf-dashboard';
+export class AppComponent implements OnInit {
+
+  chart: WritableSignal<Chart<"bar", string[], string> | null> = signal(null);
+
+  ngOnInit(): void {
+    this.loadBar();
+  }
+
+  private loadBar(): void {
+    const bar = new Chart("MyChart", {
+      type: 'bar',
+      data: {
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12', '2022-05-13',
+          '2022-05-14', '2022-05-15', '2022-05-16', '2022-05-17',],
+        datasets: [
+          {
+            label: "Sales",
+            data: ['467', '576', '572', '79', '92', '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17', '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }
+        ]
+      },
+      options: {
+        aspectRatio: 2.5
+      }
+    });
+    this.chart.set(bar)
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: AppComponent,
+  }
+];
