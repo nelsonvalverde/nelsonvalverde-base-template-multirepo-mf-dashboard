a726aa557cd4c718e5732f8be5308e65206b1a7a
Nelson Valverde La Torre <nelsonvalverdelt@aysconsultores.pe>
2024-08-11 03:01:08 -0500

    avance de dashboard tabla y grápicos pie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { PanelModule } from 'primeng/panel';
 import { MenuModule } from 'primeng/menu';
 import { CardMinimalDetailComponent } from "./components/card-minimal-detail/card-minimal-detail.component";
 import { CardMinimalDetailInterface } from './components/card-minimal-detail/card.minimal-detail.interface';
+import { TableComponent } from "./components/table/table.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AccordionModule, ChartModule, PanelModule, MenuModule, CardMinimalDetailComponent],
+  imports: [AccordionModule, ChartModule, PanelModule, MenuModule, CardMinimalDetailComponent, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -18,12 +19,12 @@ export class AppComponent implements OnInit {
   loadBarData: any;
   loadBarOptions: any;
 
-  loadBar2Data: any;
-  loadBar2Options: any;
+  loadPieData: any;
+  loadPieOptions: any;
 
   ngOnInit(): void {
     this.loadBar();
-    this.loadBar2();
+    this.loadPie();
   }
 
   private loadBar(): void {
@@ -33,19 +34,37 @@ export class AppComponent implements OnInit {
     const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
     this.loadBarData = {
-      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
       datasets: [
         {
-          label: 'Sales',
-          data: [540, 325, 702, 620],
-          backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
-          borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
-          borderWidth: 1
+          type: 'line',
+          label: 'Dataset 1',
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          borderWidth: 2,
+          fill: false,
+          tension: 0.4,
+          data: [50, 25, 12, 48, 56, 76, 42]
+        },
+        {
+          type: 'bar',
+          label: 'Dataset 2',
+          backgroundColor: documentStyle.getPropertyValue('--green-500'),
+          data: [21, 84, 24, 75, 37, 65, 34],
+          borderColor: 'white',
+          borderWidth: 2
+        },
+        {
+          type: 'bar',
+          label: 'Dataset 3',
+          backgroundColor: documentStyle.getPropertyValue('--orange-500'),
+          data: [41, 52, 24, 74, 23, 21, 32]
         }
       ]
     };
 
     this.loadBarOptions = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.6,
       plugins: {
         legend: {
           labels: {
@@ -54,86 +73,49 @@ export class AppComponent implements OnInit {
         }
       },
       scales: {
-        y: {
-          beginAtZero: true,
-          ticks: {
-            color: textColorSecondary
-          },
-          grid: {
-            color: surfaceBorder,
-            drawBorder: false
-          }
-        },
         x: {
           ticks: {
             color: textColorSecondary
           },
           grid: {
-            color: surfaceBorder,
-            drawBorder: false
+            color: surfaceBorder
+          }
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder
           }
         }
       }
     };
   }
 
-  private loadBar2(): void {
+  private loadPie(): void {
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue('--text-color');
-    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
-    this.loadBar2Data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    this.loadPieData = {
+      labels: ['A', 'B', 'C'],
       datasets: [
         {
-          label: 'My First dataset',
-          backgroundColor: documentStyle.getPropertyValue('--blue-500'),
-          borderColor: documentStyle.getPropertyValue('--blue-500'),
-          data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-          label: 'My Second dataset',
-          backgroundColor: documentStyle.getPropertyValue('--pink-500'),
-          borderColor: documentStyle.getPropertyValue('--pink-500'),
-          data: [28, 48, 40, 19, 86, 27, 90]
+          data: [540, 325, 702],
+          backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
+          hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
         }
       ]
     };
 
-    this.loadBar2Options = {
-      maintainAspectRatio: false,
-      aspectRatio: 0.8,
+    this.loadPieOptions = {
       plugins: {
         legend: {
           labels: {
+            usePointStyle: true,
             color: textColor
           }
         }
-      },
-      scales: {
-        x: {
-          ticks: {
-            color: textColorSecondary,
-            font: {
-              weight: 500
-            }
-          },
-          grid: {
-            color: surfaceBorder,
-            drawBorder: false
-          }
-        },
-        y: {
-          ticks: {
-            color: textColorSecondary
-          },
-          grid: {
-            color: surfaceBorder,
-            drawBorder: false
-          }
-        }
-
       }
     };
   }
@@ -193,5 +175,6 @@ export class AppComponent implements OnInit {
       ]
     }
   }
+
 
 }
---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,123 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { SliderModule } from 'primeng/slider';
+import { Table, TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { DropdownModule } from 'primeng/dropdown';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [TagModule, ProgressBarModule, CommonModule, TableModule, SliderModule, FormsModule, DropdownModule, MultiSelectModule, ButtonModule, InputTextModule],
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TableComponent {
+  customers = [
+    {
+      id: 1000,
+      name: 'James Butt',
+      country: {
+        name: 'Algeria',
+        code: 'dz'
+      },
+      company: 'Benton, John B Jr',
+      date: '2015-09-13',
+      status: 'unqualified',
+      verified: true,
+      activity: 17,
+      representative: {
+        name: 'Ioni Bowcher',
+        image: 'ionibowcher.png'
+      },
+      balance: 70663
+    },
+    {
+      id: 1001,
+      name: 'James Butt',
+      country: {
+        name: 'Algeria',
+        code: 'dz'
+      },
+      company: 'Benton, John B Jr',
+      date: '2015-09-13',
+      status: 'unqualified',
+      verified: true,
+      activity: 17,
+      representative: {
+        name: 'Ioni Bowcher',
+        image: 'ionibowcher.png'
+      },
+      balance: 70663
+    },
+    {
+      id: 1002,
+      name: 'James Butt',
+      country: {
+        name: 'Algeria',
+        code: 'dz'
+      },
+      company: 'Benton, John B Jr',
+      date: '2015-09-13',
+      status: 'unqualified',
+      verified: true,
+      activity: 17,
+      representative: {
+        name: 'Ioni Bowcher',
+        image: 'ionibowcher.png'
+      },
+      balance: 70663
+    },
+  ]
+
+  representatives = [
+    { name: 'Amy Elsner', image: 'amyelsner.png' },
+    { name: 'Anna Fali', image: 'annafali.png' },
+    { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+    { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+    { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+    { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+    { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+    { name: 'Onyama Limba', image: 'onyamalimba.png' },
+    { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+    { name: 'Xuxue Feng', image: 'xuxuefeng.png' }
+  ];
+
+  statuses = [
+    { label: 'Unqualified', value: 'unqualified' },
+    { label: 'Qualified', value: 'qualified' },
+    { label: 'New', value: 'new' },
+    { label: 'Negotiation', value: 'negotiation' },
+    { label: 'Renewal', value: 'renewal' },
+    { label: 'Proposal', value: 'proposal' }
+  ];
+
+  loading: boolean = false;
+
+  activityValues: number[] = [0, 100];
+
+  searchValue: string | undefined;
+
+  getSeverity(status: string): 'success' | 'danger' | 'info' | 'warning' | undefined {
+    switch (status.toLowerCase()) {
+      case 'unqualified': return 'danger';
+      case 'qualified': return 'success';
+      case 'new': return 'info';
+      case 'negotiation': return 'warning';
+      case 'renewal': return undefined;
+      default: return undefined;
+    }
+  }
+
+  clear(table: Table) {
+    table.clear();
+    this.searchValue = ''
+  }
+
+}
